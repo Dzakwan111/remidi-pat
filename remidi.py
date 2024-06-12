815337d79a05dceaf3dfc98dfa3c9c73a081c26e
dzakwan dhiaulhaq <dzakwandhiaulhaq@gmail.com>
2024-06-12 09:29:11 +0700

    remidi-pat

---
+++ remidi.py
@@ -0,0 +1,18 @@
+# # # apkdesain1 = ['canva', 'coral draw','Acrobat Pro','Photoshop','illustrator','indesign','premiere pro','after effects','lightroom','xd']
+# # # apkdesain2= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+
+# # # print ("list1[6]: ", apkdesain1[6])
+# # # print ("list2[1:10]: ",apkdesain2[1:10])
+
+# english = {'table','pen','computer','whiteboard','handphone','bag','fan','hand','keyboard','door',}
+# print(english)
+
+# # parameter a 5
+# # parameter b 7
+# # parameter t 10
+
+# # def trapesium(a = 5, b = 7, t = 10):
+# #     sum = 0.5*(a+b)*t
+# #     print('Sum:',sum)
+
+# # trapesium()
